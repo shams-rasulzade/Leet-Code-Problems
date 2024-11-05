f1576cfcecf23c8e6fc6785f15a9c6ce419455e4
Shams Rasulzade <rasulzade.sems@gmail.com>
2024-11-05 14:43:09 +0400

    added code for polyndrome

--- src/Polyndrome_Numbers/Polyndrome.java
+++ src/Polyndrome_Numbers/Polyndrome.java
@@ -1,1 +1,33 @@
-//
+package Polyndrome_Numbers;
+
+import java.util.Scanner;
+
+public class Polyndrome {
+
+    public static boolean polyndrome(int n){
+
+        int reverse = 0, original = n;
+
+        while (n!=0){
+            reverse = reverse * 10 + n%10;
+            n= n/10;
+        }
+
+        return (reverse == original);
+    }
+
+    public static void main (String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number to check: ");
+        int number = scanner.nextInt();
+        if (polyndrome(number)) {
+            System.out.println("The number " + number + " is polyndrome!");
+        }
+
+        else System.out.println("The number " + number + " is not polyndrome");
+
+    }
+
+}
