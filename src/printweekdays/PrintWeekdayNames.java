b9d727173354ed8f8347c7f6f2689c4da7815027
Shams Rasulzade <rasulzade.sems@gmail.com>
2024-11-11 16:02:52 +0400

    new: Concatenation of Array

---
+++ src/printweekdays/PrintWeekdayNames.java
@@ -0,0 +1,40 @@
+package printweekdays;
+
+import java.util.Scanner;
+
+public class PrintWeekdayNames {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        switch (num) {
+            case 1:
+                System.out.println("It's Monday!");
+                break;
+            case 2:
+                System.out.println("It's Tuesday!");
+                break;
+            case 3:
+                System.out.println("It's Wednesday!");
+                break;
+            case 4:
+                System.out.println("It's Thursday!");
+                break;
+            case 5:
+                System.out.println("It's Friday!");
+                break;
+            case 6:
+                System.out.println("It's Saturday!");
+                break;
+            case 7:
+                System.out.println("It's Sunday!");
+                break;
+            default:
+                System.out.println("There's no weekday with this index!");
+        }
+
+    }
+
+}
