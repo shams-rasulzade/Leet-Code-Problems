dfa68e8a178dc55efa28dbc2e33de3bcbd95fd6c
Shams Rasulzade <rasulzade.sems@gmail.com>
2024-11-11 15:11:58 +0400

    resolved comments

--- src/Polyndrome_Numbers/Polyndrome.java
+++ src/Polyndrome_Numbers/Polyndrome.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 public class Polyndrome {
 
-    public static boolean polyndrome(int n){
+    public static boolean polyndrome(int n) {
 
         if (n < 0) return false;
         int reverse = 0, original = n;
 
 
-        while (n != 0){
-            reverse = reverse * 10 + n%10;
-            n= n / 10;
+        while (n != 0) {
+            reverse = reverse * 10 + n % 10;
+            n = n / 10;
         }
 
         return (reverse == original);
     }
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
@@ -26,9 +26,7 @@ public class Polyndrome {
         int number = scanner.nextInt();
         if (polyndrome(number)) {
             System.out.println("The number " + number + " is polyndrome!");
-        }
-
-        else System.out.println("The number " + number + " is not polyndrome");
+        } else System.out.println("The number " + number + " is not polyndrome");
 
     }
 
