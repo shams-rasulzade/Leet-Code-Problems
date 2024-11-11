c9e697039a32d16c3039449315966aee52dd5643
Shams Rasulzade <rasulzade.sems@gmail.com>
2024-11-11 14:55:55 +0400

    resolved comments

--- src/Build_Array_from_Permutation/ArrayFromPermutation.java
+++ src/Build_Array_from_Permutation/ArrayFromPermutation.java
@@ -3,11 +3,11 @@ package Build_Array_from_Permutation;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Array_from_Permutation {
+public class ArrayFromPermutation {
 
     public static int[] buildArray(int[] nums) {
         int[] ans = new int[nums.length];
-        for (int i=0; i<nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             ans[i] = nums[nums[i]];
         }
 
@@ -17,16 +17,17 @@ public class Array_from_Permutation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the lenght of the Array: ");
+        System.out.println("Enter the length of the Array: ");
         int size = scanner.nextInt();
         int[] arr = new int[size];
 
         System.out.println("Enter the values of the Array: ");
 
-        for (int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             arr[i] = scanner.nextInt();
         }
 
-        System.out.println(Arrays.toString(buildArray(arr)));;
+        System.out.println(Arrays.toString(buildArray(arr)));
+        ;
     }
 }
