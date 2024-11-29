5b78dcb30957ff80160710a889e690d1f820995c
Shams Rasulzade <rasulzade.sems@gmail.com>
2024-11-29 15:36:11 +0400

    adding code for IntegerToRoman

--- src/Numbers/IntegertoRoman/IntergertoRoman.java
+++ src/Numbers/IntegertoRoman/IntergertoRoman.java
@@ -8,8 +8,8 @@ public class IntergertoRoman {
         int[] n = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] roman = {"M","CM","D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-
         int i = 0;
+
         String string = new String();
         while(num>0){
             if(num>=n[i]){
@@ -21,7 +21,6 @@ public class IntergertoRoman {
         }
         return string;
     }
-
 
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
