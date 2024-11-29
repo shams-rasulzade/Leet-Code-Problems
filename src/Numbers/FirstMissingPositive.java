3520ac345666000fcfba39cd176c1496dd64cc19
Shams Rasulzade <rasulzade.sems@gmail.com>
2024-11-29 13:05:16 +0400

    code for `first missing positive`

---
+++ src/Numbers/FirstMissingPositive.java
@@ -0,0 +1,27 @@
+package Numbers;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.util.Collections.min;
+
+public class FirstMissingPositive {
+
+    public static int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int min = 1;
+
+        for ( int i = 0; i < nums.length; i++){
+            if (nums[i]==min) min++;
+        }
+        return min;
+    }
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int[] nums = {7,8,9,11,12};
+        System.out.println(firstMissingPositive(nums));
+
+    }
+}
